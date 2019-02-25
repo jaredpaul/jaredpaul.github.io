1571aa24f40debac9f0d7bc3232c8e3cbf79c577
jaredpaul <jpmiller@buffalo.edu>
2019-02-25 04:23:07 -0500

    fixed background issue for mobile

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function haveBackground() {
+    var decision = true;
+    var width = $( window ).width();
+    var height = $( window ).height();
+    var ratio = parseInt(width) / parseInt(height);
+    if (ratio < (4/3)) {
+        decision = false;
+    }
+    return decision;
+}
+
+$(document).ready(function() {
+    if (haveBackground()) {
+        $('body').css('background-image', 'url("images/warwick_background.jpg")');
+        $('body').css('background-repeat', "no-repeat");
+        $('body').css('background-size', "100%");
+    }
+});
